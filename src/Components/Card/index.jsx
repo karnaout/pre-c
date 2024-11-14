28d62e240bea579b4c75fbe9f813ca29179f2a74
adfou <adfouberr@gmail.Com>
2024-11-14 21:00:16 +0100

    only build

--- src/Components/Card/index.jsx
+++
@@ -1,8 +0,0 @@
-export {CardBehavior} from "./CardBehavior"
-export  {RiskCard as ColorRiskCard} from "./CardRiskColor"
-export  {CardHumain} from "./CardHumain"
-export {CardBehaviorCheck} from "./CardBehaviorCheck"
-export {CardParticipants} from "./CardParticipants"
-export {QuestionnaireStatusCard} from "./QuestionnaireStatusCard"
-export {NextStepsCard} from "./NextStepsCard"
-export {ThreeCardsSection} from "./ThreeCardsSection"
--- src/Components/Layout/index.mjs
+++
@@ -1,4 +0,0 @@
-export {BodyPage} from "./BodyLayout.jsx"
-export {MainLayout} from "./MainLayout.jsx"
-export {AuthLayout} from "./AuthLayout.jsx"
-export {QestionnairBodyLayout} from "./QestionnairBodyLayout.jsx"
